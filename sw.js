3e76350ff356f49825d5e8ac57e35ee3ebbeaf4c
Manik Roy <pixnum@gmail.com>
2026-01-07 16:39:36 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='navmap-v1';
+self.addEventListener('install',e=>{
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
+'./','index.html','icon-192.png','icon-512.png'
+])));
+});
+self.addEventListener('fetch',e=>{
+e.respondWith(
+caches.match(e.request).then(r=>r||fetch(e.request))
+);
+});
